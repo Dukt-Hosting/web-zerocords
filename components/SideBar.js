0ff69786b5125ec6c15d3b656fd3dfc57fac8413
Joshua Ellis <joshpoodle42@gmail.com>
2021-02-08 21:49:46 -0500

    dashboard poggers

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,17 +1,28 @@
-import { Container, Nav, Row } from 'react-bootstrap'
+import { Container, Nav, Row, Button } from 'react-bootstrap'
 import styles from '../styles/Dashboard.module.css'
+import { MonetizationOn, Speed } from '@material-ui/icons';
 
 export default function SideBar(props) {
     return (
-        <Container className={styles.sidebar} style={{ width: "20%", height: "100%" }}>
+        <Container className={styles.sidenav}>
             <Row>
                 <Nav defaultActiveKey={props.activekey} className="flex-column">
-                    <Nav.Link href="/dhasboard">Active</Nav.Link>
+                    <Container style={{justifyContent: "center", padding:"2.5%"}}>
+                        <h2>
+                            ZeroCords
+                        </h2>
+                    </Container> {' '}<br/>
+                    <Container>
+                        <Button variant="primary"><Speed/> Dashboard </Button>
+                    </Container> {' '}<br/>
+                    <Container>
+                        <Button variant="primary"><MonetizationOn/> Currency </Button>
+                    </Container> {' '}<br/>
                     <Nav.Link eventKey="link-1">Link</Nav.Link>
                     <Nav.Link eventKey="link-2">Link</Nav.Link>
                     <Nav.Link eventKey="disabled" disabled>
                         Disabled
-                </Nav.Link>
+                    </Nav.Link>
                 </Nav>
             </Row>
         </Container>
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -2,26 +2,27 @@ import Head from 'next/head'
 import styles from '../../styles/Dashboard.module.css'
 import NavBar from '../../components/NavBar.js'
 import SideBar from '../../components/SideBar.js'
+import { Container } from 'react-bootstrap'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-        <Head>
-            <title>Create Next App</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <NavBar brand="ZeroCords"></NavBar>
+      <NavBar brand="ZeroCords"></NavBar>
 
-        <main className={styles.main}>
+      <main className={styles.main}>
+        <SideBar activekey="/dashboard"></SideBar>
+        <main className={styles.submain}>
 
-            <SideBar activekey="/dashboard"></SideBar>
-
-            <h1>
-                Helloooo there!
-            </h1>
-
+          <h1>
+            Helloooo there!
+          </h1>
         </main>
+      </main>
     </div>
   )
 }
