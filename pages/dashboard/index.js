45af93b04145f9f8f3d5c55bf212febf3f626ba7
Joshua Ellis <67237931+SpicyTakis@users.noreply.github.com>
2021-02-11 08:21:32 -0500

    Update index.js

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -3,7 +3,6 @@ import styles from '../../styles/Dashboard.module.css'
 import NavBar from '../../components/NavBar.js'
 import SideBar from '../../components/SideBar.js'
 import { Container } from 'react-bootstrap'
-import { useRouter } from 'next/router'
 
 export default function Home() {
   const router = useRouter()
